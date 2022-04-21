70769555b83d0b21b220b9635868fa99a72c6322
markitoo3 <dietrich.marko@gmail.com>
2022-04-21 17:49:12 +0200

    more things with hooks, classes

--- src/component/ButtonWithClass.jsx
+++ src/component/ButtonWithClass.jsx
@@ -5,19 +5,54 @@ class ButtonWithClass extends Component {
         super()
         this.state = {
             key1: "This is the default state of a class component",
-            key2: 0
+            key2: 0,
+            title: {
+                main: "main title",
+                sub: "dub title"
+            },
+            countries: []
         }
+        console.log("1");
     }
     ButtonOnClick = () => {
         this.setState({
             key1: "This is the NEW state of a class component"
         });
     }
+
+    componentDidMount() {
+        fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(countriesData => this.setState(
+            () => {
+                return {countries: countriesData}
+            },
+            () => console.log(this.state)
+        ))
+    }
+
     render() {
         return (
         <>
             <button onClick = {this.ButtonOnClick}>{this.state.key1}</button>
             <button onClick = {() => this.setState({key2: 1})}>{this.state.key2}</button>
+            <button onClick = {() =>
+                this.setState(
+                    () => {
+                        return {title: {
+                            main: "h1",
+                            sub: "satring"
+                            }  
+                        }
+                    },
+                    () => {
+                        console.log(this.state);
+                    }
+                )
+            }>{this.state.title.main}</button>
+            <div className='countries'>
+                {this.state.countries.map((country, index) => <div key={index}>{country.name.common}</div>)}
+            </div>
         </>
         )
     }
--- src/component/ButtonWithHooks.jsx
+++ src/component/ButtonWithHooks.jsx
@@ -1,13 +1,30 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function ButtonWithHooks() {
-    const [key1, setKey1] = useState("This is the default state of a function component")
-    const [key2, setKey2] = useState(0)
+  const [key1, setKey1] = useState("This is the default state of a function component")
+  const [key2, setKey2] = useState(0)
+  const [countries, setCountries] = useState([]);
+
+  
+  
+  useEffect(
+    () => {
+      console.log("render");
+      fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(countriesData => setCountries(countriesData))     
+    },
+    []
+  )
+
   return (
-    <>
+    <div>
         <button onClick={ () => setKey1("This is the NEW state of a function component")}>{key1}</button>
         <button onClick={ () => setKey2(1)}>{key2}</button>
-    </>
+        <div className='countries'>
+          {countries.map((country, index) => <div key={index}>{country.name.common}</div>)}
+        </div>
+    </div>
   )
 }
 
